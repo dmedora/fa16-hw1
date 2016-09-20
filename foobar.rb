d7a2ba7c78121441f7a263da01772d990c58d12b
dmedora <djmedora@gmail.com>
2016-09-19 19:16:57 -0700

    Add files via upload
    Completed HW1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,24 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    int_array = a.map { |i| i.to_i } # adding semicolon to also add two in this block does not seem to work.. 
+    int_array.map! { |i| i += 2 } # remember .each does not actually modify the list. 
+    int_array.keep_if { |i| i % 2 == 0 }
+    int_array.uniq!
+    int_array.keep_if { |i| i < 10 }
+    
+    int_array.reduce(0, :+) # or .inject(0, :+), or this: 
+    # sum = 0
+    # int_array.each { |i| sum += i }
+    # return sum
   end
 end
 
 
+# Foobar. Write a class function in foobar.rb called `baz` that accepts an array of integers as strings, 
+# changes each string into an integer, 
+# adds two to each number, 
+# keeps the even numbers only, 
+# removes any duplicates, 
+# rejects the resulting numbers greater than ten, 
+# and finally returns the sum of the resultingarray.
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,20 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  result = (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort! # sorts in place (destructive) (indicated by the '!')
+  a.map! {|i| i = i + 1} # map! modifies in place. map will create new array from old one. 
+
+  return a
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  combined = first_name + " " + last_name
 end
 
 def blockin_time(a)
